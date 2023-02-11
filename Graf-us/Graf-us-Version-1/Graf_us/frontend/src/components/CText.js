5b6ee98b7a2d7b6a9f57825699b7e623b7fa99b9
as☆ph <83978332+asaphkalala@users.noreply.github.com>
2023-02-11 12:04:44 +0000

    Delete Graf-us directory

--- Graf-us/Graf-us-Version-1/Graf_us/frontend/src/components/CText.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function CText(props) {
-    return (
-        <div style={{...props.textStyle, ...props.style }}>{props.children}</div>
-    );
-}
-
-export default CText;
